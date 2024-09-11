9b7a9e79ac55993ba3dfd3f32ce09d381d4bb9ee
Bunny70pl <bunny70pl@gmail.com>
2024-09-11 11:38:23 +0200

    wypisywanie i wpisywanie do tablicy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //tablica
+        //musi miec z gory okreslony rozmiar
+        //nie mozna go pozniej zmienic
+        //elementem tablicy moga byc typy proste i zlozone
+        //tablice 6 elemetnowa z wartosacmi loswymi z zkaresu 1 do 100
+        int tablicaLosowych[] =new int[6];
+        for (int i = 0; i < tablicaLosowych.length; i++) {
+            tablicaLosowych[i] = (int)(Math.random()*100+1);
+        }
+        //wypisywanie elemtnow talblicy
+        for (int element: tablicaLosowych) {
+            System.out.print(element+", ");
+        }
     }
 }
